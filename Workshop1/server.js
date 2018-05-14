c1b4ee860aa53e2649c0d7c350ced9ec88593730
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-05-14 17:40:40 -0400

    Update server.js

--- Workshop1/server.js
+++ Workshop1/server.js
@@ -1,16 +1,4 @@
 
-/*********************************************************************************
-* WEB322: Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Namra Rupesh Fanse Student ID: 112219175  Date: 14th May 2018 
-*
-* Online (Heroku) URL: https://floating-thicket-37165.herokuapp.com/
-*
-********************************************************************************/ 
-
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
