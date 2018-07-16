4efaf592fa3fc6b7eb9fab2aaabaa677dc5331e1
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-07-16 14:50:19 -0400

    Update server.js

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -1,15 +1,3 @@
-/*********************************************************************************
-* WEB322 – Assignment 04
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
-* of this assignment has been copied manually or electronically from any other source
-* (including 3rd party web sites) or distributed to other students.
-*
-* Name: Namra Rupesh Fanse Student ID: 112219175 Date: 16/7/2018
-*
-* Online (Heroku) Link: https://assignment4web322.herokuapp.com/
-*
-********************************************************************************/
-
 const express = require('express');
 const app = express();
 const path = require('path');
